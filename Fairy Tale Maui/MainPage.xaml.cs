b92be28e4f74f85228525be7264c25a0776e86dd
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-11-18 13:35:34 -0500

    Style the page

--- Fairy Tale Maui/MainPage.xaml.cs
+++ Fairy Tale Maui/MainPage.xaml.cs
@@ -1,10 +1,125 @@
-﻿namespace Fairy_Tale_Maui
+﻿using Fairy_Tale_Maui.Models;
+using Microsoft.Maui.Handlers;
+using System.Collections.ObjectModel;
+
+namespace Fairy_Tale_Maui
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<FairyTale> FairyTales { get; set; } = [];
+        public ObservableCollection<FairyTale> FairyTalesCopy { get; set; } = [];
+
         public MainPage()
         {
             InitializeComponent();
+            ModifySearchBar();
+            InitializeFairyTales();
+            BindingContext = this;
+        }
+
+        private void InitializeFairyTales()
+        {
+            FairyTales =
+            [
+                new FairyTale
+                {
+                    Name = "The Rabbit",
+                    ReadTime = new TimeSpan(0, 3, 0),
+                    IsSaved = false,
+                    Image = "bunny.png"
+                },
+
+                new FairyTale
+                {
+                    Name = "Adventure of Brave",
+                    ReadTime = new TimeSpan(0, 6, 0),
+                    IsSaved = false,
+                    Image = "bunny5.jpeg"
+                },
+
+                new FairyTale
+                {
+                    Name = "Rudy the Brave Rabbit ",
+                    ReadTime = new TimeSpan(0, 9, 0),
+                    IsSaved = false,
+                    Image = "bunny2.jpeg"
+                },
+
+                new FairyTale
+                {
+                    Name = "Misadventures Rumble",
+                    ReadTime = new TimeSpan(0, 12, 0),
+                    IsSaved = false,
+                    Image = "bunny3.jpeg"
+                },
+
+                new FairyTale
+                {
+                    Name = "Rabbit's Great Carrot",
+                    ReadTime = new TimeSpan(0, 16, 0),
+                    IsSaved = false,
+                    Image = "bunny4.jpeg"
+                },
+            ];
+
+            FairyTalesCopy =
+            [
+                new FairyTale
+                {
+                    Name = "The Rabbit",
+                    ReadTime = new TimeSpan(0, 3, 0),
+                    IsSaved = false,
+                    Image = "bunny.png"
+                },
+
+                new FairyTale
+                {
+                    Name = "Adventure of Brave",
+                    ReadTime = new TimeSpan(0, 6, 0),
+                    IsSaved = false,
+                    Image = "bunny5.jpeg"
+                },
+
+                new FairyTale
+                {
+                    Name = "Rudy the Brave Rabbit ",
+                    ReadTime = new TimeSpan(0, 9, 0),
+                    IsSaved = false,
+                    Image = "bunny2.jpeg"
+                },
+
+                new FairyTale
+                {
+                    Name = "Misadventures Rumble",
+                    ReadTime = new TimeSpan(0, 12, 0),
+                    IsSaved = false,
+                    Image = "bunny3.jpeg"
+                },
+
+                new FairyTale
+                {
+                    Name = "Rabbit's Great Carrot",
+                    ReadTime = new TimeSpan(0, 16, 0),
+                    IsSaved = false,
+                    Image = "bunny4.jpeg"
+                },
+            ];
+        }
+
+        private static void ModifySearchBar()
+        {
+            SearchBarHandler.Mapper.AppendToMapping("CustomSearchIconColor", (handler, view) =>
+            {
+#if ANDROID
+                var context = handler.PlatformView.Context;
+                var searchIconId = context.Resources.GetIdentifier("search_mag_icon", "id", context.PackageName);
+                if (searchIconId != 0) 
+                {
+                    var searchIcon = handler.PlatformView.FindViewById<Android.Widget.ImageView>(searchIconId);                    
+                    searchIcon.SetColorFilter(Android.Graphics.Color.Rgb(172, 157, 185), Android.Graphics.PorterDuff.Mode.SrcIn);                                           
+                }
+#endif
+            });
         }
     }
 
