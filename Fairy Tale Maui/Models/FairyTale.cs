6d40e042592497851e54261f13fa2a22ba373ef4
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-11-18 13:34:47 -0500

    Created model

---
+++ Fairy Tale Maui/Models/FairyTale.cs
@@ -0,0 +1,10 @@
+﻿namespace Fairy_Tale_Maui.Models
+{
+    public class FairyTale
+    {
+        public string? Name { get; set; }
+        public TimeSpan ReadTime { get; set; }
+        public bool IsSaved { get; set; }
+        public string? Image { get; set; }
+    }
+}
